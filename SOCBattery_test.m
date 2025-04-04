325db13c6b072aa5f43da57cdf62ed179d4d1299
sebahattin.babur <sebahattin.babur@FGS34ENG05>
2025-04-04 15:45:09 +0300

    new test

--- SOCBattery_test.m
+++ SOCBattery_test.m
@@ -1,32 +1,47 @@
 classdef SOCBattery_test < matlab.unittest.TestCase
-    
     properties (Access = private)
-        resultSet    % sltest.testmanager.run çıktısı (ResultSet nesnesi)
-        fileResults  % getTestFileResults(resultSet) sonucu
-        suiteResults % getTestSuiteResults(fileResults) sonucu
-        caseResults  % getTestCaseResults(suiteResults) sonucu
+        testResults
     end
     
     methods(TestClassSetup)
         function runBatterySOCTestSuite(testCase)
+            % Import the runner
+            import matlab.unittest.TestRunner
             
-            % 1) .mldatx dosyasını yükleyin
-            sltest.testmanager.load("BatterySOCSysIntTest.mldatx");
+            % Create the suite from your test file or folder
+            suite = testsuite("BatterySOCSysIntTest");
             
-            % 2) Tüm testleri çalıştırın ve sonuçları saklayın
-            testCase.resultSet = sltest.testmanager.run();
+            % Create a runner with no plugins
+            runner = TestRunner.withNoPlugins;
             
-            % 3) Sonuçları hiyerarşik olarak çekin
-            testCase.fileResults  = getTestFileResults(testCase.resultSet);
-            testCase.suiteResults = getTestSuiteResults(testCase.fileResults);
-            testCase.caseResults  = getTestCaseResults(testCase.suiteResults);
+            % Run once, store the results for use in all test methods
+            testCase.testResults = run(runner, suite);
         end
     end
     
     methods(Test)
-        function verify(testCase)
-                testCase.verifyEqual(testCase.resultSet.NumPassed,4);
+        function verify_1(testCase)
+            % Check that the 1st test in BatterySOCSysIntTest passed
+            testCase.verifyTrue(testCase.testResults(1).Passed, ...
+                'The 1st test in BatterySOCSysIntTest failed.');
+        end
+        
+        function verify_2(testCase)
+            % Check that the 2nd test in BatterySOCSysIntTest passed
+            testCase.verifyTrue(testCase.testResults(2).Passed, ...
+                'The 2nd test in BatterySOCSysIntTest failed.');
+        end
+        
+        function verify_3(testCase)
+            % Check that the 3rd test in BatterySOCSysIntTest passed
+            testCase.verifyTrue(testCase.testResults(3).Passed, ...
+                'The 3rd test in BatterySOCSysIntTest failed.');
+        end
+        
+        function verify_4(testCase)
+            % Check that the 4th test in BatterySOCSysIntTest passed
+            testCase.verifyTrue(testCase.testResults(4).Passed, ...
+                'The 4th test in BatterySOCSysIntTest failed.');
         end
     end
-        
 end
